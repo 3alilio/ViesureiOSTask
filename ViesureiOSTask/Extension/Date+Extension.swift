2a26716da138898a61f87e44fdf8fbdf47e7f699
mohamed <m.el-shawarby@nousdigital.net>
2022-06-23 20:58:18 +0200

    add date extension

---
+++ ViesureiOSTask/Extension/Date+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Date+Extension.swift
+//  ViesureTask
+//
+//  Created by Mohamed El-Shawarby on 22.06.22.
+//
+
+import Foundation
+
+extension Date {
+    
+    var veisureStringFormat: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = """
+                                EEE, MMM d, ''yy
+                                """
+        return dateFormatter.string(from: self)
+    }
+}
