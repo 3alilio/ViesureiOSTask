ddd21ebf7ece4644894efb824c51d87e0ffaf09e
mohamed <m.el-shawarby@nousdigital.net>
2022-06-23 20:10:54 +0200

    add CoreDataManager and  remove PersistenceController

---
+++ ViesureiOSTask/Utilities/CoreDataManager.swift
@@ -0,0 +1,56 @@
+//
+//  Persistence.swift
+//  ViesureiOSTask
+//
+//  Created by Mohamed El-Shawarby on 23.06.22.
+//
+
+import CoreData
+
+class CoreDataManager {
+
+    private let inMemory: Bool
+
+    init(inMemory: Bool = false) {
+        self.inMemory = inMemory
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "ViesureiOSTask", managedObjectModel: NSManagedObjectModel.mergedModel(from: [Bundle.main])!)
+        if inMemory {
+            let description = NSPersistentStoreDescription()
+            description.type = NSInMemoryStoreType
+            description.shouldAddStoreAsynchronously = false
+            description.url = URL(fileURLWithPath: "/dev/null")
+            container.persistentStoreDescriptions = [description]
+        }
+    
+        container.persistentStoreDescriptions.first?.setOption(FileProtectionType.complete as NSObject, forKey: NSPersistentStoreFileProtectionKey)
+//        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+}
